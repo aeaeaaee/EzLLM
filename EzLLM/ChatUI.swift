ff8f716922e8afd88205bc65aa9fe4d0f21dba1a
aeaeaa <aea>
2025-09-22 20:31:31 +0800

    Draft 1

--- EzLLM/ChatUI.swift
+++ EzLLM/ChatUI.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ChatUI.swift
 //  EzLLM
 //
 //  Created by Frety Yeung on 22/9/2025.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ChatUI: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ChatUI()
 }
--- EzLLM/EzLLMApp.swift
+++ EzLLM/EzLLMApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct EzLLMApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ChatUI()
         }
     }
 }
